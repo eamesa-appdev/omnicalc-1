e15af5a8a2c7e591fe79dc8883f9eb2b4c700b12
eamesa <eamadorm@chicagobooth.edu>
2022-01-31 00:54:37 +0000

    square and random work perfect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,37 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
+  def square_results
+    @square_num = params.fetch("square_input").to_f
+    @square_result = @square_num ** 2
+    render ({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+  
+  def square_root_results
+    # @square_num = params.fetch("square_input").to_f
+    # @square_result = @square_num ** 2
+    render ({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
+
+
+
+
+
+  def random_form
+    render({ :template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def random_results
+    @random_min = params.fetch("random_min").to_f
+    @random_max = params.fetch("random_max").to_f
+    @random_result = rand(@random_min..@random_max)
+    render({ :template => "calculation_templates/random_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
 
+get("/square/new",{ :controller => "application", :action =>"blank_square_form"})
+
+get("/square/results",{ :controller => "application", :action =>"square_results"})
+
+get("/random/new",{ :controller => "application", :action =>"random_form"})
+
+get("/random/results",{ :controller => "application", :action =>"random_results"})
+
+get("/square_root/new",{ :controller => "application", :action =>"blank_square_root_form"})
+
+get("/square_root/results",{ :controller => "application", :action =>"square_root_results"})
+
 end
